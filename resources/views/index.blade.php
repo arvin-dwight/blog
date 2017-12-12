e637ffdbc18f42b0b0344a09362ae36911f1706c
Arvin <dwight.antigua@gmail.com>
2017-12-13 01:13:08 +0800

    Add Angular

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <base href="/">
+        <title>Blog</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <blog-app>Loading...</blog-app>
+        <script type="text/javascript" src="{{asset('js/app.js')}}"></script>
+    </body>
+</html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,22 @@
 let mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix.webpackConfig({
+    resolve: {
+        extensions: ['.ts']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader'
+            }
+        ],
+        exprContextCritical: false
+    }
+});
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.js([
+    'resources/assets/angular/polyfills.ts',
+    'resources/assets/angular/vendor.ts',
+    'resources/assets/angular/main.ts'
+], 'public/js/app.js');
