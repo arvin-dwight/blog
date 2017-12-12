2764f962f7ef15975591ca4646dcbc954e91f48f
Arvin <dwight.antigua@gmail.com>
2017-12-13 03:18:46 +0800

    Add Material Design

--- resources/assets/angular/app/app.module.ts
+++ resources/assets/angular/app/app.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './modules/material.module';
+
 @NgModule({
     imports: [
-        BrowserModule
+        BrowserModule,
+        BrowserAnimationsModule,
+        MaterialModule
     ],
     declarations: [
         AppComponent
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,7 @@
         <title>Blog</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     </head>
     <body>
         <blog-app>Loading...</blog-app>
